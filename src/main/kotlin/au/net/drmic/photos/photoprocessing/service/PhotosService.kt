506181e12d546bc6fe12d01038d412bb05c81e16
Mic Giansiracusa <fs.micdev@gmail.com>
2019-08-25 04:52:09 +1000

    Photo cropping methods added.

--- src/main/kotlin/au/net/drmic/photos/photoprocessing/service/PhotosService.kt
+++ src/main/kotlin/au/net/drmic/photos/photoprocessing/service/PhotosService.kt
@@ -15,6 +15,9 @@ import javax.imageio.ImageIO
 import java.awt.image.BufferedImage
 import java.io.File
 import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.sql.Blob
 
 @Service
 class PhotosService {
@@ -31,21 +34,46 @@ class PhotosService {
         photos.ownerUserId = ownerUserId
         photos.datePhotoWasTaken = java.sql.Date(datePhotoWasTaken.time)
         photos.imageOriginal = SerialBlob(imageOriginal.bytes)
-        // @TODO: Crop transform to standard size and set photos.imageCroppedStandard
-        // @TODO: Crop transform to thumbnail size and set photos.imageCroppedThumbnail
+        photos.imageCroppedStandard = scale(imageOriginal, PhotoSize.WXH_512X512)
+        photos.imageCroppedThumbnail = scale(imageOriginal, PhotoSize.WXH_128X128)
         photos.dateTimeUpdated = Timestamp(Date().time)
         photos.description = description
 
         return photosRepository.save(photos)
     }
 
+    fun scale(photoMultipartFile: MultipartFile, photoSize: PhotoSize): Blob {
+        val photoName = photoSize.toString() + photoMultipartFile.originalFilename
+        val photoType = photoName.substring(photoName.indexOf(".") + 1)
+
+        val tmpFile = File(photoName)
+
+        photoMultipartFile.transferTo(tmpFile)
+        val originalBufferedImage : BufferedImage = ImageIO.read(tmpFile)
+
+        val genCroppedFile = scale(originalBufferedImage, photoSize, photoName, photoType)
+
+        val generatedBlob = SerialBlob(convertFileContentToBlob(genCroppedFile.path))
+        logger.info("Created blob " + generatedBlob + " for photoName[" + photoName + "] of photoType[" + photoType + "]")
+
+        tmpFile.delete()
+
+        return generatedBlob
+    }
+
+    @Throws(IOException::class)
+    fun convertFileContentToBlob(filePathStr: String): ByteArray {
+        val filePath = Paths.get(filePathStr)
+
+        return Files.readAllBytes(filePath)
+    }
     /**
      * Adapted from
      * https://github.com/maltesander/java-image-scaling-thumbnail/blob/master/src/main/java/com/tutorialacademy/img/imgscalr/ImageScaler.java
      */
     @Throws(IOException::class)
-    fun scale(image: BufferedImage, size: PhotoSize, imageName: String, imageType: String): File {
-        val res = size.toImageResolution()
+    fun scale(image: BufferedImage, photoSize: PhotoSize, imageName: String, imageType: String): File {
+        val res = photoSize.toImageResolution()
 
         var scaledImage: BufferedImage? = null
 
@@ -57,7 +85,7 @@ class PhotosService {
             scaledImage = image
         }
 
-        val transformedPhotoFile = File(size.toString() + "_" + imageName)
+        val transformedPhotoFile = File(photoSize.toString() + "_" + imageName)
 
         ImageIO.write(scaledImage, imageType, transformedPhotoFile)
 
